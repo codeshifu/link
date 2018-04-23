d3736af593d693b6ee48bd6c85373842b2916941
Olushi Luqman O <olushilukmon03@gmail.com>
2018-04-23 13:38:12 +0100

    add/setup dependencies

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
+import 'bootstrap3/dist/css/bootstrap.min.css'
+import $ from 'jquery'
+window.jQuery = window.$ = $
+require('bootstrap3/dist/js/bootstrap.min')
 
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
