c39e0be58299a91aa8379fe799b66aef7601f52f
Olushi Luqman O <olushilukmon03@gmail.com>
2018-04-26 14:23:06 +0100

    update component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
-import './App.css'
+import Header from './components/Header/Header'
+import Fab from './components/Fab/Fab'
+import BookmarkList from './components/BookmarkList/BookmarkList'
 
 class App extends Component {
   render () {
     return (
-      <div>
-        hello, world
+      <div style={{background: '#E8EAF6', paddingTop: 115}}>
+        <Header />
+        <BookmarkList />
+        <Fab />
       </div>
     )
   }
