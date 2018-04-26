20e9f1d976836f1e379c0620cf6bc763a7ccb05a
Olushi Luqman O <olushilukmon03@gmail.com>
2018-04-26 14:21:03 +0100

    add bookmark

--- src/components/AddBookmarkForm/AddBookmarkForm.js
+++ src/components/AddBookmarkForm/AddBookmarkForm.js
@@ -1,21 +1,45 @@
 import React, { Component } from 'react'
+import { Consumer } from '../AppContext/AppContext'
 
 class AddBookmarkForm extends Component {
+  constructor (props) {
+    super(props)
+    this.onFormSubmit = this.onFormSubmit.bind(this)
+  }
+
+  onFormSubmit (e, action) {
+    e.preventDefault()
+    const title = this.title.value
+    const link = this.url.value
+
+    if (!title || !link) return
+
+    action({title, link})
+
+    this.title.value = ''
+    this.url.value = ''
+  }
   render () {
     return (
-      <div>
-        <div className='form-group'>
-          <label htmlFor='bk_title'>Title</label>
-          <input type='text' className='form-control' id='bk_title' placeholder='e.g. apache virtualhost slow lookup' />
-        </div>
-        <div className='form-group'>
-          <label htmlFor='bk_url'>URL</label>
-          <input type='text' className='form-control' id='bk_url' placeholder='e.g. stackoverflow.com/questions/7547316/apache-virtualhost-slow-lookup' />
-        </div>
-        <div className='modal-footer row'>
-          <button type='button' className='btn btn-primary'>Add</button>
-        </div>
-      </div>
+      <Consumer>
+        {
+            (({add}) => (
+              <form onSubmit={(e) => this.onFormSubmit(e, add)}>
+                <div className='form-group'>
+                  <label htmlFor='bk_title'>Title</label>
+                  <input ref={input => { this.title = input }} type='text' className='form-control' id='bk_title' placeholder='e.g. apache virtualhost slow lookup' />
+                </div>
+                <div className='form-group'>
+                  <label htmlFor='bk_url'>URL</label>
+                  <input ref={input => { this.url = input }} type='text' className='form-control' id='bk_url' placeholder='e.g. stackoverflow.com/questions/7547316/apache-virtualhost-slow-lookup' />
+                </div>
+                <div className='modal-footer row'>
+                  <button type='submit' className='btn btn-primary'>Add</button>
+                </div>
+              </form>
+            ))
+        }
+      </Consumer>
     )
   }
 }
