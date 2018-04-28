26da3a69fd9486eac790404432d0d189311e2634
Olushi Luqman O <olushilukmon03@gmail.com>
2018-04-28 17:31:07 +0100

    removed unused prop typechecking

--- src/components/AddBookmark/AddBookmark.js
+++ src/components/AddBookmark/AddBookmark.js
@@ -1,26 +1,17 @@
 import React from 'react'
 import { Consumer } from '../AppContext/AppContext'
 import AddEditBookmarkForm from '../AddEditBookmarkForm/AddEditBookmarkForm'
-import PropTypes from 'prop-types'
 
-const AddBookmark = ({bookmark}) => {
+const AddBookmark = () => {
   return (
     <Consumer>
       {
           (({add}) => (
             <AddEditBookmarkForm
-              bookmark={bookmark}
-              onSubmit={(data) => add(data)} />
-                  )
-              )
+              onSubmit={(data) => add(data)} />))
           }
     </Consumer>
   )
-}
-
-AddBookmark.propTypes = {
-  bookmark: PropTypes.object.isRequired,
-  onSubmit: PropTypes.func.isRequired
 }
 
 export default AddBookmark
