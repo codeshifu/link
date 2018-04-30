204e106cf2fa45ec20febe66a008930d1ba91a46
Olushi Luqman O <olushilukmon03@gmail.com>
2018-04-30 12:31:48 +0100

    refactored options menu into its own component

---
+++ src/components/OptionsMenu/OptionsMenu.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const OptionsMenu = ({editModalId, onDelete}) => {
+  return (
+    <div className='dropdown'>
+      <span className='btn dropdown-toggle' data-toggle='dropdown'>
+        <span className='glyphicon glyphicon-option-vertical' aria-hidden='true' />
+      </span>
+      <ul className='dropdown-menu dropdown-menu-right'>
+        <li role='presentation'>
+          <a role='menuitem' data-toggle='modal' data-target={`#${editModalId}`}>Edit</a>
+        </li>
+        <li role='presentation'>
+          <a role='menuitem'>Copy URL</a>
+        </li>
+        <li className='divider' />
+        <li role='presentation'>
+          <a role='menuitem' onClick={onDelete}>Delete</a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default OptionsMenu
