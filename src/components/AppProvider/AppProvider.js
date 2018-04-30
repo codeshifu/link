e2dfd33db3a1268f49d83470afd0344f6e48b46f
Olushi Luqman O <olushilukmon03@gmail.com>
2018-04-30 12:32:16 +0100

    add sample tags

--- src/components/AppProvider/AppProvider.js
+++ src/components/AppProvider/AppProvider.js
@@ -9,11 +9,33 @@ class AppProvider extends Component {
       bookmarks: {
         bookmark1: {
           title: 'Creating your own snippets',
-          link: 'https://code.visualstudio.com/docs/editor/userdefinedsnippets'
+          link: 'https://code.visualstudio.com/docs/editor/userdefinedsnippets',
+          tags: ['vscode']
         },
         bookmark2: {
           title: 'Apache VirtualHost slow lookup',
-          link: 'https://stackoverflow.com/questions/7547316/apache-virtualhost-slow-lookup'
+          link: 'https://stackoverflow.com/questions/7547316/apache-virtualhost-slow-lookup',
+          tags: ['apache']
+        },
+        bookmark3: {
+          title: 'Empty View for Android\'s RecyclerView',
+          link: 'http://akbaribrahim.com/empty-view-for-androids-recyclerview/',
+          tags: ['android', 'recyclerview', 'empty-view']
+        },
+        bookmark4: {
+          title: 'HTML Currency Symbols, Entities and Codes — HTML Arrows',
+          link: 'http://htmlarrows.com/currency/',
+          tags: ['html']
+        },
+        bookmark5: {
+          title: 'PHP file_get_contents with variables - Stack Overflow',
+          link: 'http://stackoverflow.com/questions/39688416/php-file-get-contents-with-variables',
+          tags: ['php']
+        },
+        bookmark6: {
+          title: 'osx - Access denied for user root - mysql on MAC OS - Stack Overflow',
+          link: 'http://stackoverflow.com/a/36252036/4238544',
+          tags: ['mac', 'osx']
         }
       }
     }
