bbecad736ce99cebfc7b1d8051caca0d84b9ded1
Olushi Luqman O <olushilukmon03@gmail.com>
2018-05-01 03:42:01 +0100

    render filtered bookmarks

--- src/components/BookmarkList/BookmarkList.js
+++ src/components/BookmarkList/BookmarkList.js
@@ -1,15 +1,26 @@
 import React from 'react'
 import Bookmark from '../Bookmark/Bookmark'
 import {Consumer} from '../AppContext/AppContext'
+import filters from '../../filters/bookmarkFilter'
+import './bookmarkList.css'
 
 const BookmarkList = ({bookmarks = []}) => {
   return (
-    <Consumer>
-      {
-        ({state}) => Object.keys(state.bookmarks).map(id =>
-          <Bookmark key={id} tags={state.bookmarks[id].tags} bookmark={{...state.bookmarks[id], id}} />)
+    <div className='_bookmarkList'>
+      <Consumer>
+        {
+        ({bookmarks, searchQuery}) => {
+          const filteredBookmarks = filters(bookmarks, searchQuery)
+          if (filteredBookmarks.length <= 0) return <p style={{background: '#fff', margin: 0}} className='text-center'>No search results found</p>
+          return filteredBookmarks.map(id =>
+            <Bookmark
+              key={id}
+              tags={bookmarks[id].tags}
+              bookmark={{...bookmarks[id], id}} />)
         }
-    </Consumer>
+      }
+      </Consumer>
+    </div>
   )
 }
 
