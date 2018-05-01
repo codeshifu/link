9074469dc90659ebbd849b51f521e0fc68b0d692
Olushi Luqman O <olushilukmon03@gmail.com>
2018-05-01 03:41:09 +0100

    dispatch action to provider on search

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react'
 import './search.css'
+import { Consumer } from '../AppContext/AppContext'
 
 class Search extends Component {
+  constructor (props) {
+    super(props)
+    this.onSearch = this.onSearch.bind(this)
+  }
+
+  onSearch (action) {
+    const query = this.searchInput.value
+    action(query)
+  }
+
   render () {
     return (
-      <div className='_search'>
-        <input type='text' className='form-control' placeholder='search bookmarks' />
-      </div>
+      <Consumer>
+        {
+          ({search}) => (
+            <div className='_search'>
+              <input
+                ref={input => { this.searchInput = input }}
+                onChange={() => this.onSearch(search)}
+                type='text'
+                className='form-control'
+                placeholder='search bookmarks' />
+            </div>
+          )
+        }
+      </Consumer>
     )
   }
 }
