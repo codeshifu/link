76af89440172b753be4cfcce31d7fb9aa15c7337
Olushi Luqman O <olushilukmon03@gmail.com>
2018-05-01 13:04:37 +0100

    consume modal id from provider

--- src/components/Fab/Fab.js
+++ src/components/Fab/Fab.js
@@ -2,18 +2,24 @@ import React from 'react'
 import './fab.css'
 import Modal from '../Modal/Modal'
 import AddBookmark from '../AddBookmark/AddBookmark'
+import { Consumer } from '../AppContext/AppContext'
 
 const Fab = () => {
-  const modalId = 'add_bookmark'
   return (
-    <React.Fragment>
-      <Modal modalId={modalId} modalTitle='Add bookmark'>
-        <AddBookmark />
-      </Modal>
-      <div className='_fab' data-toggle='modal' data-target={`#${modalId}`}>
-        <img src='/images/bookmark.svg' alt='add bookmark' />
-      </div>
-    </React.Fragment>
+    <Consumer>
+      {
+        (({addBookmarkId}) => (
+          <React.Fragment>
+            <Modal modalId={addBookmarkId} modalTitle='Add bookmark'>
+              <AddBookmark />
+            </Modal>
+            <div className='_fab' data-toggle='modal' data-target={`#${addBookmarkId}`}>
+              <img src='/images/bookmark.svg' alt='add bookmark' />
+            </div>
+          </React.Fragment>
+        ))
+      }
+    </Consumer>
   )
 }
 
